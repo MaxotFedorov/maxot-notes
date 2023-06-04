94886a5489a25b0857a07f2f179a5a2584fb7175
MaxotFedorov <maxot007@gmail.com>
2023-06-04 23:19:58 +0300

    add editing notes

--- maxot_notes/main/urls.py
+++ maxot_notes/main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('create', views.create, name='create')
+    path('create', views.create, name='create'), 
+    path('note_<int:pk>', views.NoteDetailView.as_view(), name='note'),
+    path('note_<int:pk>/edit', views.NoteUpdateView.as_view(), name='note_edit')
+    
 ]
