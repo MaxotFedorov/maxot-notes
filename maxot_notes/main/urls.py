69fa2196009fbdf5ec7937ec063da4b829656150
MaxotFedorov <maxot007@gmail.com>
2023-07-01 19:50:58 +0300

    Add registration

--- maxot_notes/main/urls.py
+++ maxot_notes/main/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('create', views.create, name='create'), 
+    path('create', views.create, name='create'),
     path('note_<int:pk>', views.NoteDetailView.as_view(), name='note'),
     path('note_<int:pk>/edit', views.NoteUpdateView.as_view(), name='note_edit'),
-    path('search', views.SearchListView.as_view(), name='search'),    
+    path('search', views.SearchListView.as_view(), name='search'),
+    path('signup', views.SignUp.as_view(), name='signup'),
 ]
