013cf108316c4854a1cf92e55f8d190940a6954c
MaxotFedorov <maxot007@gmail.com>
2023-07-02 23:13:33 +0300

    Add profile pict

--- maxot_notes/maxot_notes/urls.py
+++ maxot_notes/maxot_notes/urls.py
@@ -23,5 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('users/', include('users.urls')),
-    path('', include('main.urls')),    
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', include('main.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
