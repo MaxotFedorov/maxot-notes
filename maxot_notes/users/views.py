6acd2d07a5128aeaf631fafdcc9892ec7866a5fa
MaxotFedorov <maxot007@gmail.com>
2023-07-03 18:29:29 +0300

    User can update profile pict

--- maxot_notes/users/views.py
+++ maxot_notes/users/views.py
@@ -3,17 +3,29 @@ from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView, DetailView
 from django.contrib.auth.models import User
-
+from users.forms import ProfileUpdateForm
 
 
 def user(request, username):
+    form = None
     user = User.objects.get(username=username)
-    return render(request, 'users/user.html', {'received_user' : user})
+    if user.username == request.user.username:
+        if request.method == 'POST':
+            form = ProfileUpdateForm(request.POST, 
+                                     request.FILES, 
+                                     instance=user.profile)
+            if form.is_valid():
+                form.save()
+        form = ProfileUpdateForm()
+    context = {'received_user' : user, 
+               'form' : form}
+    return render(request, 'users/user.html', context)
 
 
 def all_users(request):
     user = User.objects.all()
     return render(request, 'users/all_users.html', {'received_users' : user})
+
 
 class SignUp(CreateView):
     form_class = UserCreationForm
